0626faaf13affcf3d8336bb86c73d99ee6880a15
sirinya810 <27837@ud.ac.th>
2026-09-18 12:41:30 +0700

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -9,10 +9,3 @@
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
 price = int(input("ราคาสินค้า :"))
 if price >=2000:
-    price = price - 200
-    print(f"ราคาสุทธิที่ต้องชำระคือ {price}")
-elif price >=1000:
-    price = price - 100
-    print(f"ราคาสุทธิที่ต้องชำระคือ {price}")
-else:
-    print(f"ราคาสุทธิที่ต้องชำระคือ {price}")
